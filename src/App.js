967279836ddafd8c5b6d529bcf5bba1bdbf07771
Ariel Febrian <arielfebrian98@gmail.com>
2022-03-30 21:59:27 +0700

    add navbar and assests

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react'
 
+import { About, Header, Footer, Skills, Testimonial, Work} from './container'
+import { Navbar } from './components'
+import './App.scss'
+
 export default function App() {
   return (
-    <div>App</div>
+    <div className='app'>
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
+    </div>
   )
 }
